9d3ce4f88a1421271948cbace3a63add569cdb55
unknown <nthienchuong@tma.com.vn>
2019-04-20 16:03:52 +0700

    fix tags format

--- infra/aws_vpc.tf
+++ infra/aws_vpc.tf
@@ -5,24 +5,20 @@ vpc_cidr_block           = "${var.vpc_cidr_block}"
 vpc_instance_tenancy     = "${var.vpc_instance_tenancy}"
 vpc_enable_dns_support   = "${var.vpc_enable_dns_support}"
 vpc_enable_dns_hostnames = "${var.vpc_enable_dns_hostnames}"
-vpc_tags = {
-  "Name" = "${var.vpc_tags}"
-}
+vpc_tags =  "${var.vpc_tags}"
+
 #Subnet block
 subnet_vpc_id_cidrblock        = "${var.subnet_vpc_id_cidrblock}"
 subnet_map_public_ip_on_launch = "${var.subnet_map_public_ip_on_launch}"
 subnet_availability_zone       = "${var.subnet_availability_zone}"
-subnet_tags = {
-  "Name" = "${var.subnet_tags}"
-}
+subnet_tags = "${var.subnet_tags}"
+
 #Internet gateway block
-internet_gateway_tags = {
-  "Name" = "${var.internet_gateway_tags}"
-}
+internet_gateway_tags  = "${var.internet_gateway_tags}"
+
 #Route table block
-route_table_tags = {
-  "Name" = "${var.route_table_tags}"
-}
+route_table_tags = "${var.route_table_tags}"
+
 #Internet access block
 internet_access_destination_cidr_block = "${var.internet_access_destination_cidr_block}"
 }
