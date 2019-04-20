c08c5236d1a3c5b3337d7a2ba47af94444c109db
unknown <nthienchuong@tma.com.vn>
2019-04-20 15:54:12 +0700

    fix tags format

--- module/aws_vpc/main.tf
+++ module/aws_vpc/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "My_VPC" {
   instance_tenancy     = "${var.vpc_instance_tenancy}"
   enable_dns_support   = "${var.vpc_enable_dns_support}"
   enable_dns_hostnames = "${var.vpc_enable_dns_hostnames}"
-  tags                 = "${var.vpc_tags}"
+  tags                 =  {"Name" = "${var.vpc_tags}"}
 }
 #End Create VPC
 
@@ -19,21 +19,25 @@ resource "aws_subnet" "My_VPC_Subnet" {
   vpc_id                  = "${aws_vpc.My_VPC.id}"
   map_public_ip_on_launch = "${var.subnet_map_public_ip_on_launch}"
   availability_zone       = "${var.subnet_availability_zone}"
-tags = "${var.subnet_tags}"
+tags = {
+  "Name" = "${var.subnet_tags}"
+  }
 }
 #End Create subnet
 
 #Create Internet gateway
 resource "aws_internet_gateway" "My_VPC_GW" {
-  vpc_id = "${aws_vpc.My_VPC.id}"
-tags     = "${var.internet_gateway_tags}"
+  vpc_id   = "${aws_vpc.My_VPC.id}"
+  tags     = {
+    "Name" = "${var.internet_gateway_tags}"
+  }
 }
 #End Create Internet gateway
 
 #Create Route table
 resource "aws_route_table" "My_VPC_route_table" {
   vpc_id = "${aws_vpc.My_VPC.id}"
-tags = "${var.route_table_tags}"
+  tags = { "Name" = "${var.route_table_tags}" }
 }
 #End Route table
 
