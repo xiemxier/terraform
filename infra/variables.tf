642c8be1c1b9609b4ee191a279bbe6b084f5bfe0
unknown <nthienchuong@tma.com.vn>
2019-04-20 15:56:17 +0700

    fix tags format

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,7 +3,7 @@ variable "vpc_cidr_block" {}
 variable "vpc_instance_tenancy" {}
 variable "vpc_enable_dns_support" {}
 variable "vpc_enable_dns_hostnames" {}
-variable "vpc_tags" {type = "string"}
+variable "vpc_tags" {type = "map"}
 #End VPC block
 
 #Subnet block
