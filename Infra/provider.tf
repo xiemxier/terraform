5addaa24dd23656e941907c40cfcebac757b3e08
nguyenlkgauden <nguyenlk2006@gmail.com>
2019-04-22 22:02:57 +0700

    Update provider.tf
    change

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAIRQ6SAP7PPTHDTRA"
-  secret_key = "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"
+  region = 
+  access_key = 
+  secret_key = 
 
 }
