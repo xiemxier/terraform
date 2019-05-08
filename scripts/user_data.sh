9fbaf41e8338047531ff21d8cd26eb571a49a214
Soulblade <vuphuoc91@gmail.com>
2019-05-08 23:02:54 +0700

    Add script install user data 4

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -3,11 +3,11 @@ yum update -y
 # Install wget, git & apache
 yum -y install httpd wget git
 # Install yum tools
-yum install yum-utils
+yum -y install yum-utils
 # Install php 7.2
 yum-config-manager --enable remi-php72
-yum install php php-mysql php-common php-json php-zip
+yum -y install php php-mysql php-common php-json php-zip
 # Install MySQL server
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum install mysql-server
+yum -y install mysql-server
