9390b5c4266afffba878f22b7c0c523e4834072f
unknown <nthienchuong@tma.com.vn>
2019-06-16 21:37:54 +0700

    edit target id in aws lb target group attachment

--- infra/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  value = "${module.aws_instance.ip_address_public}"
-}
-output "aws_instance_private_ip" {
-  value = "${module.aws_instance.aws_instance_private_ip}"
-}
-output "aws_instance_ids" {
-  value = "${module.aws_instance.aws_instance_id}"
-}
